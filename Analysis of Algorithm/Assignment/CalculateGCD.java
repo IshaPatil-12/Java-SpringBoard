44bad37eb8ee15f7862da470b191b0ca132628bc
Isha Patil <ishapatil120306@gmail.com>
2025-10-02 20:50:02 +0530

    Analysis of Algorithm assignment

---
+++ Analysis of Algorithm/Assignment/CalculateGCD.java
@@ -0,0 +1,17 @@
+public static int calculateGCD(int num1, int num2) {
+	int temp;
+	if (num1%num2 ==0) 
+		return num2;
+	if (num1 < num2) {
+		temp=num1;
+		num1=num2;
+		num2=temp;
+	}
+	while (num2 > 0) {
+		num1 = num1%num2;
+		temp=num1;
+		num1=num2;
+		num2=temp;
+	}
+	return num1;
+}
