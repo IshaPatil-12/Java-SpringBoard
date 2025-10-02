9a83158fa609caca7cbbee615b06686eaf559f45
Isha Patil <ishapatil120306@gmail.com>
2025-10-02 20:45:42 +0530

    Analysis of Algorithm exercise

---
+++ Analysis of Algorithm/Exercise/GetCount.java
@@ -0,0 +1,19 @@
+public static int GetCount(int arr[], int n) { 
+	int count1 = 0; 
+	int count2 = 0;
+	int index;
+	for (int index1 = 0; index1 < n - 1; index1++) 
+		for (int index2 = index1 + 1; index2 < n; index2++) 
+			if (arr[index1] > arr[index2]) 
+				count1++; 
+			else {
+				index=index2;
+				count2=0;
+				while(index>index1) {
+					count2++;
+					index--;
+				}
+			}
+	return Math.max(count1, count2); 
+}
+
